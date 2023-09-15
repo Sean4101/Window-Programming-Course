bbbc818ea1be817e22754a054fa7b1f09aced473
Sean4101 <seanchen1014@gmail.com>
2023-09-15 16:29:17 +0800

    Practice 1-1 basic structure

--- 9_15/Practice 1-1/Practice 1-1/Program.cs
+++ 9_15/Practice 1-1/Practice 1-1/Program.cs
@@ -10,6 +10,66 @@ namespace Practice_1_1
     {
         static void Main(string[] args)
         {
+            int userAction;
+            bool programEnded = false;
+
+            while (!programEnded)
+            {
+                Console.WriteLine("(1) Product List (2) Add to Cart (3) Remove from Cart (4) View Cart (5) Calculate Total Amount (6) Leave Website");
+                Console.WriteLine("Select action by entering number:");
+
+                userAction = int.Parse(Console.ReadLine());
+
+                switch (userAction)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        programEnded = true;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private void ProductList()
+        {
+
+        }
+
+        private void AddToCart()
+        {
+
+        }
+
+        private void RemoveFromCart()
+        {
+
+        }
+
+        private void ViewCart()
+        {
+
+        }
+
+        private void CalculateTotalAmount()
+        {
+
+        }
+
+        private void LeaveWebsite()
+        {
+
         }
     }
 }
